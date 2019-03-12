6dd6b37559a5f69f0a48668a894bbd7d7128ab44
Felix Kurth <it@lan1.de>
2019-03-12 10:05:24 +0000

    Dev

--- action-commands.py
+++ action-commands.py
@@ -3,71 +3,40 @@
 
 import configparser
 from hermes_python.hermes import Hermes
-from hermes_python.ontology import *
 import io
-import random
-import os
+from lan1commands import Lan1Commands
 
-CONFIGURATION_ENCODING_FORMAT = "utf-8"
-CONFIG_INI = "config.ini"
+USERNAME_INTENTS = "lan1hotspots"
 
-class SnipsConfigParser(configparser.SafeConfigParser):
-    def to_dict(self):
-        return {section: {option_name: option for option_name, option in self.items(section)} for section in
-                self.sections()}
+def user_intent(intentname):
+    return USERNAME_INTENTS + ":" + intentname
 
 def read_configuration_file(configuration_file):
     try:
-        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
-            conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
-            return conf_parser.to_dict()
-    except (IOError, configparser.Error) as e:
+        cp = configparser.ConfigParser()
+        with io.open(configuration_file, encoding="utf-8") as f:
+            cp.read_file(f)
+        return {section: {option_name: option for option_name, option in cp.items(section)}
+                for section in cp.sections()}
+    except (IOError, configparser.Error):
         return dict()
 
 def subscribe_intent_callback(hermes, intent_message):
-    conf = read_configuration_file(CONFIG_INI)
-    action_wrapper(hermes, intent_message, conf)
-
-def random_line(afile):
-    line = next(afile)
-    for num, aline in enumerate(afile, 2):
-        if random.randrange(num):
-            continue
-        line = aline
-    return line
-
-def action_wrapper(hermes, intent_message, conf):
-    """ Write the body of the function that will be executed once the intent is recognized. 
-    In your scope, you have the following objects : 
-    - intent_message : an object that represents the recognized intent
-    - hermes : an object with methods to communicate with the MQTT bus following the hermes protocol. 
-    - conf : a dictionary that holds the skills parameters you defined. 
-      To access global parameters use conf['global']['parameterName']. 
-      For end-user parameters use conf['secret']['parameterName'] 
-
-    Refer to the documentation for further details. 
-    """
-
-    if(len(intent_message.slots.name) >= 1):
-        name = str(intent_message.slots.name.first().value)
-    
-        file = open(os.path.dirname(os.path.realpath(__file__)) + "/sprueche.txt")
-        line = random_line(file)
-        file.close()
-    
-        if (name.lower() in conf["secret"]["safe_names"]):
-            result_sentence = "Das hättest du gerne. {} lässt dir ausrichten: {}".format(name, line)
-        else:
-            result_sentence = "Hey {}, {}".format(name, line)
-    else:
-        result_sentence = "Ich habe leider nicht verstanden wen ich beschimpfen soll."
-
-    current_session_id = intent_message.session_id
-    hermes.publish_end_session(current_session_id, result_sentence)
-
+    # conf = read_configuration_file(CONFIG_INI)
+    intentname = intent_message.intent.intent_name
+	if intentname == user_intent("Alarm"):
+		result_sentence = shoppinglist.set_alarm(intent_message)
+		hermes.publish_end_session(intent_message.session_id, result_sentence)
+	elif intentname == user_intent("Aktivate_Object"):
+		result_sentence = shoppinglist.activate_object(intent_message)
+		hermes.publish_end_session(intent_message.session_id, result_sentence)
+	elif intentname == user_intent("Deaktivate_Object"):
+		result_sentence = shoppinglist.deactivate_object(intent_message)
+		hermes.publish_end_session(intent_message.session_id, result_sentence)
 
 if __name__ == "__main__":
+    config = read_configuration_file("config.ini")
+    lan1commands = Lan1Commands(config)
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("DANBER:beschimpfe", subscribe_intent_callback).start()
+        h.subscribe_intents(subscribe_intent_callback).start()
 
---
+++ lan1commands.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import pickle
+import random
+import io
+from ast import literal_eval
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import datetime
+import socket
+
+
+class Lan1Commands:
+    def __init__(self, config):
+        self.config = config
+        self.wanted_intents = []  # For reacting only with wanted intents
+
+	def set_alarm(self, intentmessage):
+		response= "OK"
+		return response
+	def activate_object(self, intentmessage):
+		if(len(intent_message.slots.object) >= 1):
+			response = "OK"
+		else:
+			response = "Entschuldigung. Ich habe leider nicht Verstanden was ich anschalten soll."
+		return response
+	def deactivate_object(self, intentmessage):
+		if(len(intent_message.slots.object) >= 1):			
+			response = "OK"
+		else:
+			response = "Entschuldigung. Ich habe leider nicht Verstanden was ich ausschalten soll."
+		return response
+
+
